690e787374d20ba92bd3ac2df8ab26d4adcd494c
unknown <dboroday2018@gmail.com>
2026-05-15 00:07:57 +0300

    hero ui polish

--- app/components/CatchAttention.tsx
+++ app/components/CatchAttention.tsx
@@ -2,10 +2,10 @@
 
 export default function CatchAttention() {
     return (
-        <section className='w-full h-full'>
-            <h2 className='blue text-5xl lg:text-9xl mt-16 lg:mt-48 ml-8 lg:ml-12'>we make it faster!</h2>
-            <p className='blue_dark text-5xl lg:text-9xl text-end mr-8 lg:mr-24 mt-12 lg:mt-24'>we make it better!!</p>
-            <p className='cyan text-5xl lg:text-9xl mt-12 lg:mt-24 lg:text-center ml-12 lg:ml-0'>we make it perfect!!!</p>
+        <section className='w-full h-full bg-black'>
+            <h2 className='blue text-5xl lg:text-[200px] mt-16 lg:mt-24 ml-8 lg:ml-12'>we make it faster!</h2>
+            <p className='blue_dark text-5xl lg:text-[200px] text-end mr-8 lg:mr-24 mt-12 lg:mt-24'>we make it better!!</p>
+            <p className='cyan text-5xl lg:text-[200px] mt-12 lg:mt-24 lg:text-center ml-12 lg:ml-0'>we make it perfect!!!</p>
         </section>
     )
 }
