2a19e0bc0373dfa28181be8fd83c3521ad053f68
unknown <dboroday2018@gmail.com>
2026-05-15 18:58:47 +0300

    hero ui design

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,9 +2,9 @@
 
 export default function Hero() {
     return (
-        <section className='w-full h-screen lg:h-full flex flex-col justify-center lg:items-center  mb-12 lg:mb-24 pl-4 lg:pl-0'>
-            <p className='title text-9xl lg:text-[520px] mt-2 lg:mt-0'>pure</p>
-            <h1 className='subtitle text-3xl lg:text-6xl text-end pr-4 lg:pr-32 tracking-widest mt-12 lg:mt-0 w-full'>we build<br className='lg:hidden' /> brands</h1>
+        <section className='w-full h-full flex flex-col justify-center lg:items-center  mb-6 lg:mb-24 pl-4 lg:pl-0'>
+            <p className='title text-8xl lg:text-[520px] mt-2 lg:mt-0'>pure</p>
+            <h1 className='subtitle text-2xl lg:text-6xl text-end pr-4 lg:pr-32 tracking-widest mt-12 lg:mt-0 w-full'>we build<br className='lg:hidden' /> brands</h1>
 
 
 
